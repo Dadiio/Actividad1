48df613e91a9cf6b932027a514a83250ea5737b0
Dadiio <piccoro_daimaku@hotmail.com>
2023-01-09 13:20:17 -0400

    Ultimo

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,30 +1,30 @@
 import React, { useState } from "react";
 import "./CardCss.css"
-import axios from "axios";
 import Modal from "./Modal";
 
 
 const Card =({libro}) =>{
   const [mostrar, setMostrar]=useState(false);
-  const [itemLibro,setItem] = useState();
+  const [itemLibro,setItem] = useState(); 
+  console.log(itemLibro) 
   return (
     <>
     {
       libro.map((item)=>{
         let miniImg =item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
         let precio = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
-        if(miniImg!= undefined && precio!=undefined) 
+        if(miniImg != undefined && precio !=undefined) 
         {
           return (
             <>
-            <div className="card" onClick={()=>{setMostrar(true)}}>
+            <div className="card" onClick={()=>{setMostrar(true);setItem(item)}}>
               <img src={miniImg} alt="#" />
               <div className="bottom">
                 <h3 className="titulo">{item.volumeInfo.title}</h3>
                 <p className="precio">US&#65284;{precio}</p>
               </div>
             </div> 
-            <Modal />
+            <Modal show={mostrar} item={itemLibro} onClose={() => setMostrar(false)} />
             </>
           )
         }        
--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import './ModalCss.css'
 
-const Modal=(mostrar,item) =>{
+const Modal=({show,item,onClose}) =>{
+    if (!show)
+    {
+        return null;
+    }
+    let miniImg =item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
     return (
         <>
             <div className="overlay">
                 <div className="overlay-inner">
-                    <button className="cerrar">
+                    <button className="cerrar" onClick={onClose}>
                         <i class="fas fa-times" />
                     </button>
                     <div className="inner-box">
-                        <img src="./images/portada.png" alt="#" />
+                        <img src={miniImg} alt="#" />
                         <div className="info">
-                            <h1>PRueba React</h1><br/>
-                            <h3>otra prueba</h3><br/>
-                            <h4> otra mas <span>2016-04-21</span></h4><br/>
-                            <a href="#"><button>Mas</button></a>
+                            <h1>{item.volumeInfo.title}</h1><br/>
+                            <h3>{item.volumeInfo.authors}</h3><br/>
+                            <h4> {item.volumeInfo.publisher}<span> {item.volumeInfo.publishedDate}</span></h4><br/>
+                            <h4>ISBN 13 = {item.volumeInfo.industryIdentifiers[0].identifier} </h4> 
+                            <h4>ISBN 10 = {item.volumeInfo.industryIdentifiers[1].identifier} </h4> 
+                            <a href={item.volumeInfo.previewLink}><button>Mas</button></a>   
+                                                    
                         </div>
                     </div>
-                    <h4 className="descripcion">lo que sea</h4>
+                    <h4 className="descripcion">{item.volumeInfo.description}</h4>
                 </div>
             </div>
         </>
