79b3001d8dc0e445e1cbcf8758d1182453b2888a
Dadiio <piccoro_daimaku@hotmail.com>
2023-01-08 22:47:21 -0400

    nuevo

---
+++ src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card =() =>{
+  return (
+    <>
+    <div className="card">
+        <img src="./images/books4.jpeg" alt="#" />
+    </div>
+    </>
+  )
+}
+
+export default Card
---
+++ src/Components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './HeaderCss.css';
+
+const Header =() => {
+  return (
+    <div className="header">
+        <div className="fila1">
+            <h1> Biblioteca de Prestamos de la Actividad 1.</h1>
+        </div>
+        <div className="fila2">
+            <h2>Buscar Libros</h2>
+            <div className="busqueda">
+                <input type="text" placeholder="Digite el nombre del libro" />
+                <button><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+            <img src="./images/books4.jpeg" alt="#" />
+        </div>
+    </div>    
+  );
+}
+
+export default Header;
