cab0315f924b551be0b5852fbbea1aaab1bb0c0f
Dadiio <piccoro_daimaku@hotmail.com>
2023-01-09 11:51:35 -0400

    OverLay

---
+++ src/Components/Modal.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './ModalCss.css'
+
+const Modal=() =>{
+    return (
+        <>
+            <div className="overlay">
+                <div className="overlay-inner">
+                    <button className="cerrar">
+                        
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Modal;
